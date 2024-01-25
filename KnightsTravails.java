16748d08d3ff259894a370f5e1573b372159d3e9
davidgallego <david.gall.01@hotmail.com>
2024-01-25 11:52:54 -0500

    Add knight movements array

--- KnightsTravails.java
+++ KnightsTravails.java
@@ -1,7 +1,13 @@
 class KnightsTravails {
 
+    private static final int[][] knightMovements = { { 2, -1 }, { 1, -2 }, { -1, -2 }, { -2, -1 }, { -2, 1 }, { -1, 2 },
+            { 1, 2 }, { 2, 1 } };
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Create adjacency list
+        // Create BFS function
+        // Use BFS function to find shortest path from source to target
+        // Print shortest path
     }
 
 }
